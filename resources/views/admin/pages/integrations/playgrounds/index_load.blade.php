dc66d8d04b679a606591257b14aadc1b59540c15
Leonardo DEV <contato@innsystem.com.br>
2025-03-29 16:13:01 -0300

    Melhroias

--- resources/views/admin/pages/integrations/playgrounds/index_load.blade.php
+++ resources/views/admin/pages/integrations/playgrounds/index_load.blade.php
@@ -42,16 +42,15 @@
 <div class="row">
     @foreach($productOffers as $item)
     <div class="col-6 col-md-4 col-lg-3 mb-4">
-        <img src="{{ $item['image'] }}" alt="{{ $item['name'] }}" class="img-fluid border rounded mb-2 p-1">
+        <a href="{{ $item['offer_link'] }}" target="_Blank"><img src="{{ $item['image'] }}" alt="{{ $item['name'] }}" class="img-fluid border rounded mb-2 p-1"></a>
 
-        <p class="mb-1 fs-7 fw-bold">{{ $item['name'] }}</p>
+        <p class="mb-1 fs-7 fw-bold"><a href="{{ $item['offer_link'] }}" target="_Blank">{{ $item['name'] }}</a></p>
         <p class="mb-1 fs-7 fw-bold">ID: {{ $item['id'] }}</p>
         <p class="mb-1 fs-7 fw-bold">Categories: {{ json_encode($item['categories']) }}</p>
         <p class="mb-0 fs-7 text-muted">Preço: R$ {{ $item['price_min'] }} - R$ {{ $item['price_max'] }}</p>
         <p class="mb-0 fs-7 text-muted">Comissão: {{ $item['commission'] * 100 }}%</p>
         <div class="d-flex gap-3 mt-2">
-            <a href="{{ $item['offer_link'] }}" class="btn btn-sm {{ $item['existyItem'] ? 'btn-secondary' : 'btn-success'}} fs-7 p-1" target="_Blank"><i class="fas fa-link"></i> Abrir</a>
-            <button type="button" class="btn btn-sm btn-success fs-7 p-1 button-create-product" data-product-id="{{$item['id']}}" data-product-name="{{$item['name']}}" data-product-images="{{$item['image']}}" data-product-categories="{{json_encode($item['categories'])}}" data-product-price-min="{{$item['price_min']}}" data-product-price-max="{{$item['price_max']}}" data-product-link="{{ $item['offer_link'] }}"><i class="fa fa-{{ $item['existyItem'] ? 'history' : 'check-square'}}"></i> {{ $item['existyItem'] ? 'Atualizar' : 'Cadastrar'}}</button>
+            <button type="button" class="btn w-100 btn-sm btn-success fs-7 p-1 button-create-product" data-product-id="{{$item['id']}}" data-product-name="{{$item['name']}}" data-product-images="{{$item['image']}}" data-product-categories="{{json_encode($item['categories'])}}" data-product-price-min="{{$item['price_min']}}" data-product-price-max="{{$item['price_max']}}" data-product-link="{{ $item['offer_link'] }}"><i class="fa fa-{{ $item['existyItem'] ? 'history' : 'check-square'}}"></i> {{ $item['existyItem'] ? 'Atualizar' : 'Cadastrar'}}</button>
         </div>
     </div>
     @endforeach
