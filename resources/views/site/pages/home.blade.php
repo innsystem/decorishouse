bb6f5f8918265ef417eec0ee0b0d0953bf51f86d
Leonardo DEV <contato@innsystem.com.br>
2025-04-01 10:51:00 -0300

    Ajustes

--- resources/views/site/pages/home.blade.php
+++ resources/views/site/pages/home.blade.php
@@ -10,36 +10,36 @@
         </div>
 
         <div class="row g-12">
-            @foreach($promotionsRecents as $product_link)
+            @foreach($promotionsRecents as $product_link_promotion)
             <div class="col-xxl-2 col-lg-3 col-sm-4 col-6">
                 <div class="product-card h-100 p-8 border border-gray-100 hover-border-main-600 rounded-16 position-relative transition-2">
-                    <a href="{{ $product_link->affiliate_link }}" data-product-link-id="{{$product_link->id}}" data-affiliate-link="{{ $product_link->affiliate_link }}" class="product-link-href product-card__thumb flex-center">
-                        <img src="{{ asset('/galerias/img_loading.gif') }}" data-src="{{ $product_link->product->images[0] }}" alt="" class="lazy-load" loading="lazy">
+                    <a href="{{ $product_link_promotion->affiliate_link }}" data-product-link-id="{{$product_link_promotion->id}}" data-affiliate-link="{{ $product_link_promotion->affiliate_link }}" class="product-link-href product-card__thumb flex-center">
+                        <img src="{{ asset('/galerias/img_loading.gif') }}" data-src="{{ $product_link_promotion->product->images[0] }}" alt="" class="lazy-load" loading="lazy">
                     </a>
                     <div class="product-card__content p-sm-2">
                         <h6 class="title text-lg fw-semibold mt-12 mb-8">
-                            <a href="{{ $product_link->affiliate_link }}" data-product-link-id="{{$product_link->id}}" data-affiliate-link="{{ $product_link->affiliate_link }}" class="product-link-href link text-line-2">{{$product_link->product->name}}</a>
+                            <a href="{{ $product_link_promotion->affiliate_link }}" data-product-link-id="{{$product_link_promotion->id}}" data-affiliate-link="{{ $product_link_promotion->affiliate_link }}" class="product-link-href link text-line-2">{{$product_link_promotion->product->name}}</a>
                         </h6>
                         <div class="flex-align gap-4">
-                            <span class="text-main-600 text-md d-flex"><img src="{{ asset('/galerias/img_loading.gif') }}" data-src="{{ asset('/galerias/icons/marketplaces/'.$product_link->integration->slug.'.png') }}" alt="" class="avatar-marketplace lazy-load" loading="lazy"></span>
-                            <span class="text-gray-500 text-xs"><b>{{$product_link->integration->name}}</b> <i class="text-muted">{{ $product_link->product->created_at->diffForHumans() }}</i></span>
+                            <span class="text-main-600 text-md d-flex"><img src="{{ asset('/galerias/img_loading.gif') }}" data-src="{{ asset('/galerias/icons/marketplaces/'.$product_link_promotion->integration->slug.'.png') }}" alt="" class="avatar-marketplace lazy-load" loading="lazy"></span>
+                            <span class="text-gray-500 text-xs"><b>{{$product_link_promotion->integration->name}}</b> <i class="text-muted">{{ $product_link_promotion->product->created_at->diffForHumans() }}</i></span>
                         </div>
 
                         <div class="product-card__price mb-8 d-flex align-items-center gap-8 mt-10">
-                            @if($product_link->product->price_promotion > $product_link->product->price)
+                            @if($product_link_promotion->product->price_promotion > $product_link_promotion->product->price)
                             <span class="text-heading text-md fw-semibold">
-                                R$ {{ number_format($product_link->product->price, 2, ',', '.') }}
+                                R$ {{ number_format($product_link_promotion->product->price, 2, ',', '.') }}
                             </span>
                             <span class="text-gray-500 fw-normal">~</span>
                             @endif
 
                             <span class="text-heading text-md fw-semibold">
-                                R$ {{ number_format($product_link->product->price_promotion, 2, ',', '.') }}
+                                R$ {{ number_format($product_link_promotion->product->price_promotion, 2, ',', '.') }}
                             </span>
                         </div>
 
                         <div class="product-card__content mt-12">
-                            <a href="{{ $product_link->affiliate_link }}" data-product-link-id="{{$product_link->id}}" data-affiliate-link="{{ $product_link->affiliate_link }}" class="product-link-href product-card__cart btn bg-main-50 text-main-600 hover-bg-main-600 hover-text-white py-11 px-24 rounded-pill flex-align gap-8 mt-24 w-100 justify-content-center">
+                            <a href="{{ $product_link_promotion->affiliate_link }}" data-product-link-id="{{$product_link_promotion->id}}" data-affiliate-link="{{ $product_link_promotion->affiliate_link }}" class="product-link-href product-card__cart btn bg-main-50 text-main-600 hover-bg-main-600 hover-text-white py-11 px-24 rounded-pill flex-align gap-8 mt-24 w-100 justify-content-center">
                                 Ver Promoção <i class="ph ph-heart"></i>
                             </a>
                         </div>
