763233c618bdd790dc4a34ee1caa07ed533473e1
Leonardo DEV <contato@innsystem.com.br>
2025-04-13 11:45:58 -0300

    Ajustes

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -626,13 +626,13 @@ class ProductService
 
 		$queryParams = [
 			'token'             => 'm7ThIZbEzdquOsY57IAvoSS6k1ZTdrLZ1u760QZuUF13gHfOLHGA5YWH0dtqccCT',
-			'facebook_meta_id'  => 60,
+			'facebook_meta_id'  => 61,
 			'name'              => $product->name,
 			'content'           => $content,
 			'media'             => $social_image,
 			'local'             => ['instagram_post', 'facebook_post'],
-			'mark_product'      => 1,
-			'catalog_id'        => '1359397078637160',
+			'mark_product'      => 0,
+			'catalog_id'        => '',
 			'retailer_id'       => $product_id,
 
 		];
