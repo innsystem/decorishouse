3da606dbebdd64207e649f488e4b11adf7c30b36
Leonardo DEV <contato@innsystem.com.br>
2025-04-15 15:50:52 -0300

    Novos Ajustes

--- app/Messages/GeneralMessages.php
+++ app/Messages/GeneralMessages.php
@@ -6,6 +6,7 @@ return [
             'subject' => 'Bem-vindo ao sistema',
             'body' => 'Olá {{name}}, seja bem-vindo!',
         ],
+        
         'password_recovery' => [
             'subject' => 'Recuperação de Senha',
             'body' => 'Foi solicitada uma recuperação de senha para seu e-mail. <br> Digite o código abaixo para redefinir sua senha: <br> {{password_code}}',
@@ -14,10 +15,11 @@ return [
 
     'whatsapp' => [
         'welcome' => 'Olá {{name}}, seja bem-vindo!',
+
         'password_recovery' => "Olá {{name}}, foi solicitada uma recuperação de senha para seu e-mail. \nDigite o código abaixo para redefinir sua senha: \n*{{password_code}}*",
         'product_send_image' => "{{image}}",
         'product_send_link' => "{{link}}",
-        
+
         'product_send_image_group' => "{{image}}",
         'product_send_link_group' => "✨ *{{title}}* \n\n🔸 Preço: R$ {{price}} \n📲 Ver mais\n{{link}}",
     ],
